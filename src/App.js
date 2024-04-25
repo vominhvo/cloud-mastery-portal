c8bec7da8b6a21737c41db01f0f2a2b6cffd0e8a
tmtam612 <tmtam612@gmail.com>
2024-04-25 08:14:36 +0700

    update image from internet to local repo

--- src/App.js
+++ src/App.js
@@ -5,27 +5,27 @@ const Home = () => {
         {
             id: 1,
             title: 'hats',
-            imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+            imageUrl: '/hats.png',
         },
         {
             id: 2,
             title: 'jackets',
-            imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+            imageUrl: '/jackets.png',
         },
         {
             id: 3,
             title: 'sneakers',
-            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+            imageUrl: '/sneakers.png',
         },
         {
             id: 4,
             title: 'womens',
-            imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+            imageUrl: '/womens.png',
         },
         {
             id: 5,
             title: 'mens',
-            imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+            imageUrl: '/men.png',
         },
     ];
 
